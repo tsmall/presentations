b45b0b228fce6c299e460a3fc2a826758e4585f5
Tom Small <tom@offerpop.com>
2015-07-21 21:28:13 -0400

    Change config to a 16:9 ratio

--- presentation/config.js
+++ presentation/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  width: 1000,
-  height: 700,
+  width: 1600,
+  height: 900,
   margin: 40,
   theme: require("../themes/default/index"),
   print: require("../themes/default/print"),
